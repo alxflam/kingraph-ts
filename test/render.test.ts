74c23be6da301127fd2526dc6c0babeb19a6856f
alxflam <alex.flammer.dev@outlook.com>
2024-08-21 18:05:27 +0200

    test: update render tests due to new drawDirection option

--- test/render.test.ts
+++ test/render.test.ts
@@ -3,12 +3,12 @@ import render from '../src/render.js';
 
 describe('Should Render', () => {
   test('Should render DOT', async () => {
-    const result = await render({ families: [], people: {}, styles: {} }, { format: 'dot', theme: 'dark' });
+    const result = await render({ families: [], people: {}, styles: {} }, { format: 'dot', theme: 'dark', drawDirection: 'LR' });
     expect(result).toMatch(new RegExp('digraph G {.*}$', 's'));
   });
 
   test('Should render SVG', async () => {
-    const result = await render({ families: [], people: {}, styles: {} }, { format: 'svg', theme: 'dark' });
+    const result = await render({ families: [], people: {}, styles: {} }, { format: 'svg', theme: 'dark', drawDirection: 'LR' });
     expect(result).toMatch(new RegExp('^<svg.*</svg>$', 's'));
   });
 });
