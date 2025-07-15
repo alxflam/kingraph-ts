733cb35544a02bfb139b2acf55a4831b5afc9445
alxflam <alex.flammer.dev@outlook.com>
2025-07-16 00:02:41 +0200

    style: apply formatting

--- test/render.test.ts
+++ test/render.test.ts
@@ -3,12 +3,18 @@ import render from '../src/render.js';
 
 describe('Should Render', () => {
   test('Should render DOT', async () => {
-    const result = await render({ families: [], people: {}, styles: {} }, { format: 'dot', theme: 'dark', drawDirection: 'LR', ancestorGraph: false, ancestorLeaf: undefined });
+    const result = await render(
+      { families: [], people: {}, styles: {} },
+      { format: 'dot', theme: 'dark', drawDirection: 'LR', ancestorGraph: false, ancestorLeaf: undefined }
+    );
     expect(result).toMatch(new RegExp('digraph G {.*}$', 's'));
   });
 
   test('Should render SVG', async () => {
-    const result = await render({ families: [], people: {}, styles: {} }, { format: 'svg', theme: 'dark', drawDirection: 'LR', ancestorGraph: false, ancestorLeaf: undefined });
+    const result = await render(
+      { families: [], people: {}, styles: {} },
+      { format: 'svg', theme: 'dark', drawDirection: 'LR', ancestorGraph: false, ancestorLeaf: undefined }
+    );
     expect(result).toMatch(new RegExp('^<svg.*</svg>$', 's'));
   });
 });
